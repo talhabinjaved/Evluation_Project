25c03bae2bc9d2bcf160d0c188743e74e6fadc6e
Talha Javed <talha.javed@techverx.com>
2020-12-24 16:02:39 +0500

    Changes in tricks model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_24_104242) do
+ActiveRecord::Schema.define(version: 2020_12_24_105703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_12_24_104242) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["challenge_id"], name: "index_tricks_on_challenge_id"
+    t.index ["customer_id", "challenge_id"], name: "index_tricks_on_customer_id_and_challenge_id", unique: true
     t.index ["customer_id"], name: "index_tricks_on_customer_id"
   end
 
