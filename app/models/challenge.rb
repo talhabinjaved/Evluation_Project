bdf83b751a55fdbfbd46099810f4135075e090c3
Talha Javed <talha.javed@techverx.com>
2020-12-29 20:53:26 +0500

    attachemnt added

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,4 +2,6 @@ class Challenge < ApplicationRecord
   belongs_to :brand
   has_many :tricks
   has_many :customers, through: :tricks
+  has_one_attached :image
+  has_one_attached :video
 end
--- db/migrate/20201224105703_adduniqueindex.rb
+++ db/migrate/20201224105703_adduniqueindex.rb
@@ -1,6 +1,5 @@
 class Adduniqueindex < ActiveRecord::Migration[6.1]
   def change
     add_index :tricks, [:customer_id, :challenge_id], unique: true
-
   end
 end
