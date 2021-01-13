090d146bb7a3492e7325b8ed4b42ab9aa6a86f04
TalhaJaved <talhajaved02@gmail.com>
2021-01-13 19:38:31 +0500

    changes

--- app/models/trick.rb
+++ app/models/trick.rb
@@ -1,5 +1,5 @@
 class Trick < ApplicationRecord
-    validates :body, presence: true, length: { minimum: 10 }
+    validates :body, presence: true, length: { minimum: 10 }  
     belongs_to :customer
     belongs_to :challenge
     has_one :reward, dependent: :destroy
